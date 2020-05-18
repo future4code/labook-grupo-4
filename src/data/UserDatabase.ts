20728dfa7cba841fcc8635c0b72bd55d249bd2cd
Nadia Carvalho <carvalho.nadiacaroline@gmail.com>
2020-05-18 17:33:46 -0300

    finalizado

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -2,10 +2,10 @@ import { BaseDataBase } from "./BaseDatabase";
 import {v4} from "uuid";
 
 export class UserDatabase extends BaseDataBase{
-    tableName: string = "Users_arq";
+  private static USER_TABLE_NAME = "user_labook"
 
     public async signup(id:string, name: string, email: string, password:string):Promise<void>{
-        return await super.getConnection().raw(`INSERT INTO Users_arq (id, name, email)
+        return await super.getConnection().raw(`INSERT INTO ${UserDatabase.USER_TABLE_NAME} (id, name, email, password)
         VALUES ('${id}', '${name}', '${email}', '${password}')`);
     }
 
